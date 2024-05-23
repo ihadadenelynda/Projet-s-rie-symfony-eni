b5555607c9d2a4f499cf1d3f3cfb5d115c9e81f5
Lynda IHADADENE <lynda.oumaouche@hotmail.fr>
2024-05-23 06:14:44 +0200

    creation du service

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -1,8 +1,12 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Serie;
+use App\Form\SerieType;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,33 +15,99 @@ use Symfony\Component\Routing\Attribute\Route;
 class SerieController extends AbstractController
 {
     #[Route('', name: 'list')]
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
+        $series = $serieRepository->findBestSeries();
 
-        //todo: aller chercher les series en BDD
-        return $this->render('serie/list.html.twig', [
+        return $this->render('serie/list.html.twig', [ "series" => $series]);
 
-        ]);
     }
 
 #[Route('/details/{id}', name: 'details')]
 
-public function details(int $id): Response
+public function details(int $id,SerieRepository $serieRepository): Response
 {
-    //todo: aller chercher les series en BDD
-    return $this->render('serie/details.html.twig', [
+    $serie = $serieRepository->find($id);
 
-    ]);
+    if(!$serie) {
+        throw $this ->createNotFoundException('oh no!!!');
+
+    }
+
+    return $this->render('serie/details.html.twig',[
+        "serie" => $serie
+]);
 }
 
     #[Route('/create', name: 'create')]
 
-    public function create(): Response
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
     {
-        //todo: aller chercher les series en BDD
-        return $this->render('serie/create.html.twig', [
+        $serie = new Serie();
+        $serie->setDateCreated(new \DateTime());
 
-        ]);
+        $serieForm = $this->createForm(SerieType::class, $serie);
+
+        $serieForm->handleRequest($request);
+
+        if($serieForm->isSubmitted() && $serieForm->isValid()) {
+
+            $entityManager->persist($serie);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Serie added! Good job.');
+            return $this->redirectToRoute('serie_details', ['id' => $serie-> getId()]);
+        }
+
+
+        return $this->render('serie/create.html.twig',[
+        'serieForm' => $serieForm->createView()]);
     }
 
+    #[Route('/demo', name: 'em-demo')]
+    public function demo(EntityManagerInterface $entityManager): Response
+    {
+        //Créer une instance de mon entité
+        $serie = new Serie();
+        //Hydrater toutes les propriétés
+        $serie->setName('pif');
+        $serie-> setBackdrop('dafsd');
+        $serie->setPoster('dafsdf');
+        $serie->setDateCreated(new \DateTime());
+        $serie->setFirstAirDate(new \DateTime("-1 year"));
+        $serie->setLastAirDate(new \DateTime("6 months"));
+        $serie->setGenres('drama');
+        $serie->setOverview('bla bla bla');
+        $serie->setPopularity(123.00);
+        $serie->setVote(8.2);
+        $serie->setStatus('canceled');
+        $serie->setTmdbId("329432");
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        //$entityManager->remove($serie);
+        $serie->setGenres('comedy');
+        $entityManager->flush();
+
+
+
+        return $this->render(view: 'serie/create.html.twig');
+    }
+
+#[Route('/delete/{id}', name: 'delete')]
+public function delete(Serie $serie, EntityManagerInterface $entityManager)
+{
+
+    $entityManager->remove($serie);
+    $entityManager->flush();
+    return $this->redirectToRoute('main_home');
+}
 }
