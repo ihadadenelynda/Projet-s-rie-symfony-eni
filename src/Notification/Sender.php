f91a86cf27395f90b71aef41ee3e257b9add9e19
Lynda IHADADENE <lynda.oumaouche@hotmail.fr>
2024-05-23 13:59:38 +0200

    gestion des erreurs

--- src/Notification/Sender.php
+++ src/Notification/Sender.php
@@ -1,8 +1,26 @@
 <?php
 
 namespace App\Notification;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class Sender
 {
 
+
+    public function __construct(protected MailerInterface $mailer)
+    {
+    }
+public function sendNewUserNotificationToAdmin(UserInterface $user) :void
+    {
+        //pour tester
+       // file_put_contents('debug.txt', $user->getUserIdentifier());
+        $message = new Email();
+        $message->from('acounts@series.com')
+                ->to('admin@series.com')
+                ->subject('new account created on series.com')
+                ->html('<h1>New account!</h1>email: ' . $user->getUserIdentifier());
+        $this->mailer->send($message);
+    }
 }
